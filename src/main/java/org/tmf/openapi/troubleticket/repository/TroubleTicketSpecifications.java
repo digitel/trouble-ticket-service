8116019d19ff866d071688557ce51c9fc42ea318
kapil gidwani <kapil.gidwani@gmail.com>
2018-01-03 19:55:03 +0100

    specification file missing

---
+++ src/main/java/org/tmf/openapi/troubleticket/repository/TroubleTicketSpecifications.java
@@ -0,0 +1,28 @@
+package org.tmf.openapi.troubleticket.repository;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.tmf.openapi.troubleticket.domain.Status;
+import org.tmf.openapi.troubleticket.domain.TroubleTicket;
+
+@SuppressWarnings("serial")
+public class TroubleTicketSpecifications {
+
+	public static Specification<TroubleTicket> status(final Status status) {
+
+		return new Specification<TroubleTicket>() {
+
+			@Override
+			public Predicate toPredicate(Root<TroubleTicket> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+
+				Predicate ttStatus = cb.equal(root.get("status"), status);
+				return ttStatus;
+			}
+		};
+	}
+
+}
